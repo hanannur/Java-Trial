d92d60651fda3ad4ba6e4a9ed8aa5b91fd0afe4c
Hanan-Nuru <hanannuru65@gmail.com>
2025-12-08 23:03:38 +0300

    Additional examples

---
+++ try/DataSender.java
@@ -0,0 +1,12 @@
+import java.net.*;  
+public class DataSender{  
+  public static void main(String[] args) throws Exception {  
+    DatagramSocket ds = new DatagramSocket();  
+    String str = "This is a packet";  
+    InetAddress ip = InetAddress.getByName("127.0.0.1");  
+     
+    DatagramPacket dp = new DatagramPacket(str.getBytes(), str.length(), ip, 3000);  
+    ds.send(dp);  
+    ds.close();  
+  }  
+}  
---
+++ try/URLConnectionDemo.java
@@ -0,0 +1,15 @@
+import java.io.*;  
+    import java.net.*;  
+    public class URLConnectionDemo {  
+    public static void main(String[] args){  
+    try{  
+    URL url=new URL("https://www.tutorialspoint.com/java/java_networking.htm");  
+    URLConnection urlcon=url.openConnection();  
+    InputStream stream=urlcon.getInputStream();  
+    int i;  
+    while((i=stream.read())!=-1){  
+    System.out.print((char)i);  
+    }  
+    }catch(Exception e){System.out.println(e);}  
+    }  
+    }  
